d557158f4f352d1a53b4b2f47fc8ee0a68b1c54c
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-06 19:21:14 +0530

    Basic router is setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home />),
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      {/* <LoginPage /> */}
-      <SignupPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
