d691806604c099178d54f08e42a40145f90f82c5
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-06 18:37:30 +0530

    Login and signup page design is created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      {/* <LoginPage /> */}
+      <SignupPage />
     </div>
   );
 }
